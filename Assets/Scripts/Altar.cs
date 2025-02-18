d3a23ce2ad7eb9ccfbfb99654341326f42dacf0a
Finikfin <finikgrib@gmail.com>
2025-02-18 19:33:54 +0300

    доделал алтарь

--- Assets/Scripts/Altar.cs
+++ Assets/Scripts/Altar.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Altar : MonoBehaviour
 {
-    public string Upgrade = "Upgrade";
+    public string Upgrade; 
+    public GameObject pressEText; 
+
+    private bool isPlayerInRange = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Invoke("LoadNextScene", 2f);
+            isPlayerInRange = true;
+            pressEText.SetActive(true); 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            pressEText.SetActive(false); 
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            LoadNextScene();
         }
     }
 
