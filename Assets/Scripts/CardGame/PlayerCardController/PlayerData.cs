d8e5868ef24d4d736fd6c840998215d99344acad
DukeOfTimes <107131066+DukeTime@users.noreply.github.com>
2025-02-21 21:43:51 +0300

    cursed mask

--- Assets/Scripts/CardGame/PlayerCardController/PlayerData.cs
+++ Assets/Scripts/CardGame/PlayerCardController/PlayerData.cs
@@ -7,7 +7,7 @@ public static class PlayerData
 {
     public static List<ConsumableData> StartConsumables = new List<ConsumableData>()
     {
-        AllConsumables.All.Find(m => m.id == "2"),
+        AllConsumables.All.Find(m => m.id == "4"),
         AllConsumables.All.Find(m => m.id == "2"),
         AllConsumables.All.Find(m => m.id == "1"),
         AllConsumables.All.Find(m => m.id == "3"),
