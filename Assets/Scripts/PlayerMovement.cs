33b08434219a287d70e57f648789861e716f5a9e
Finikfin <finikgrib@gmail.com>
2025-02-21 03:03:26 +0300

    добавил анимацию персонажа

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,53 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Animator animator;
+
+    private Vector2 movement;
+    private int lastDirection = 0; // 0: idle, 1: up, 2: down, 3: left, 4: right
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
+
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal"); 
-        float moveY = Input.GetAxis("Vertical");  
-        Vector2 movement = new Vector2(moveX, moveY);
-        rb.linearVelocity = movement * moveSpeed;
+        // Получаем ввод от игрока
+        float moveX = Input.GetAxisRaw("Horizontal"); // Используем GetAxisRaw для дискретного ввода
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        // Ограничиваем движение по диагонали
+        if (moveX != 0 && moveY != 0)
+        {
+            moveY = 0; // Игнорируем вертикальное движение, если есть горизонтальное
+        }
+
+        movement = new Vector2(moveX, moveY).normalized;
+
+        // Управление анимацией
+        if (movement.magnitude > 0)
+        {
+            if (moveY > 0) lastDirection = 1; // Вверх
+            else if (moveY < 0) lastDirection = 2; // Вниз
+            else if (moveX < 0) lastDirection = 3; // Влево
+            else if (moveX > 0) lastDirection = 4; // Вправо
+        }
+        else
+        {
+            lastDirection = 0; // Без движения
+        }
+
+        animator.SetInteger("Direction", lastDirection);
+    }
+
+    private void FixedUpdate()
+    {
+        // Применяем движение
+        rb.velocity = movement * moveSpeed;
     }
 }
